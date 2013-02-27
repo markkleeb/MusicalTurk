284f44e78a7dadefbae6663389369a3fa06f31ff
markkleeb <kleeb930@gmail.com>
2013-02-26 20:19:42 -0500

    first

---
+++ app.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+import os, datetime, re
+from flask import Flask, request, render_template, redirect, abort
+from werkzeug import secure_filename
+
+# import all of mongoengine
+from flask.ext.mongoengine import mongoengine
+
+# import data models
+import models
+
+# Amazon AWS library
+import boto
+
+# Python Image Library
+import StringIO
+
+
+app = Flask(__name__)   # create our flask app
+app.secret_key = os.environ.get('SECRET_KEY') # put SECRET_KEY variable inside .env file with a random string of alphanumeric characters
+app.config['CSRF_ENABLED'] = False
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 megabyte file upload
+
+# --------- Database Connection ---------
+# MongoDB connection to MongoLab's database
+mongoengine.connect('mydata', host=os.environ.get('MONGOLAB_URI'))
+app.logger.debug("Connecting to MongoLabs")
+
+ALLOWED_EXTENSIONS = set(['wav', 'mp3', 'aac'])
+
+
+# --------- Routes ----------
+
+# this is our main page
+@app.route("/", methods=['GET','POST'])
+def index():
+
+	# get Idea form from models.py
+	song_upload_form = models.song_upload_form(request.form)
+	
+	# if form was submitted and it is valid...
+	if request.method == "POST" and song_upload_form.validate():
+		
+		uploaded_file = request.files['fileupload']
+		# app.logger.info(file)
+		# app.logger.info(file.mimetype)
+		# app.logger.info(dir(file))
+		
+		# Uploading is fun
+		# 1 - Generate a file name with the datetime prefixing filename
+		# 2 - Connect to s3
+		# 3 - Get the s3 bucket, put the file
+		# 4 - After saving to s3, save data to database
+
+		if uploaded_file and allowed_file(uploaded_file.filename):
+			# create filename, prefixed with datetime
+			now = datetime.datetime.now()
+			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
+			# thumb_filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
+
+			# connect to s3
+			s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
+			# open s3 bucket, create new Key/file
+			# set the mimetype, content and access control
+			b = s3conn.get_bucket(os.environ.get('AWS_BUCKET')) # bucket name defined in .env
+			k = b.new_key(b)
+			k.key = filename
+			k.set_metadata("Content-Type", uploaded_file.mimetype)
+			k.set_contents_from_string(uploaded_file.stream.read())
+			k.make_public()
+
+			# save information to MONGO database
+			# did something actually save to S3
+			if k and k.size > 0:
+				
+				submitted_loop = models.Loop()
+				submitted_loop.title = request.form.get('title')
+				submitted_loop.postedby = request.form.get('postedby')
+				submitted_loop.filename = filename # same filename of s3 bucket file
+				submitted_loop.save()
+
+
+			return redirect('/')
+
+		else:
+			return "uhoh there was an error " + uploaded_file.filename
+
+
+
+	else:
+		# get existing images
+		songs = models.Loop.objects.order_by('-timestamp')
+		
+		# render the template
+		templateData = {
+			'songs' : songs,
+			'form' : song_upload_form
+		}
+
+		return render_template("main.html", **templateData)
+
+@app.route('/delete/<loopid>')
+def delete_image(loopid):
+	
+	song = models.Loop.objects.get(id=loopid)
+	if song:
+
+		# delete from s3
+	
+		# connect to s3
+		s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
+		# open s3 bucket, create new Key/file
+		# set the mimetype, content and access control
+		bucket = s3conn.get_bucket(os.environ.get('AWS_BUCKET')) # bucket name defined in .env
+		k = bucket.new_key(bucket)
+		k.key = image.filename
+		bucket.delete_key(k)
+
+		# delete from Mongo	
+		song.delete()
+
+		return redirect('/')
+
+	else:
+		return "Unable to find requested image in database."
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.lower().rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+# --------- Server On ----------
+# start the webserver
+if __name__ == "__main__":
+	app.debug = True
+	
+	port = int(os.environ.get('PORT', 5000)) # locally PORT 5000, Heroku will assign its own port
+	app.run(host='0.0.0.0', port=port)
+
+
+
+	
---
+++ models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from flask.ext.mongoengine.wtf import model_form
+from wtforms.fields import * # for our custom signup form
+from flask.ext.mongoengine.wtf.orm import validators
+from flask.ext.mongoengine import *
+from datetime import datetime
+
+
+class Loop(mongoengine.Document):
+
+	title = mongoengine.StringField(max_length=120, required=True)
+	postedby = mongoengine.StringField(max_length=120, required=True, verbose_name="Your name")
+	
+	tags = mongoengine.ListField( mongoengine.StringField())
+
+	filename = mongoengine.StringField()
+
+	# Comments is a list of Document type 'Comments' defined above
+
+	# Timestamp will record the date and time idea was created.
+	timestamp = mongoengine.DateTimeField(default=datetime.now())
+
+
+song_form = model_form(Loop)
+
+# Create a WTForm form for the photo upload.
+# This form will inhirit the Photo model above
+# It will have all the fields of the Photo model
+# We are adding in a separate field for the file upload called 'fileupload'
+class song_upload_form(song_form):
+	fileupload = FileField('Upload a clip', validators=[])
+
+
+	
+
