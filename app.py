3d2bdf8322a94d0d1356f66fa07722bde9900550
markkleeb <kleeb930@gmail.com>
2013-04-16 22:41:36 -0400

    json

--- app.py
+++ app.py
@@ -324,6 +324,78 @@ def hits():
 		return render_template("hits.html", **templateData)
 
 
+@app.route("/upload", methods=['GET','POST'])
+def upload():
+
+
+
+	# get Idea form from models.py
+	song_upload_form = models.song_upload_form(request.form)
+	
+	# if form was submitted and it is valid...
+	if request.method == "POST" and song_upload_form.validate():
+		
+		uploaded_file = request.files['fileupload']
+		# app.logger.info(file)
+		# app.logger.info(file.mimetype)
+		# app.logger.info(dir(file))
+		
+		# Uploading is fun
+		# 1 - Generate a file name with the datetime prefixing filename
+		# 2 - Connect to s3
+		# 3 - Get the s3 bucket, put the file
+		# 4 - After saving to s3, save data to database
+
+		if uploaded_file and allowed_file(uploaded_file.filename):
+			# create filename, prefixed with datetime
+			now = datetime.datetime.now()
+			filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
+			# thumb_filename = now.strftime('%Y%m%d%H%M%s') + "-" + secure_filename(uploaded_file.filename)
+
+			# connect to s3
+			s3conn = boto.connect_s3(os.environ.get('AWS_ACCESS_KEY_ID'),os.environ.get('AWS_SECRET_ACCESS_KEY'))
+
+			# open s3 bucket, create new Key/file
+			# set the mimetype, content and access control
+			b = s3conn.get_bucket(os.environ.get('AWS_BUCKET')) # bucket name defined in .env
+			k = b.new_key(b)
+			k.key = filename
+			k.set_metadata("Content-Type", uploaded_file.mimetype)
+			k.set_contents_from_string(uploaded_file.stream.read())
+			k.make_public()
+
+			# save information to MONGO database
+			# did something actually save to S3
+			if k and k.size > 0:
+				
+				submitted_loop = models.Song()
+				submitted_loop.title = request.form.get('title')
+				submitted_loop.postedby = request.form.get('postedby')
+				submitted_loop.tag = request.form.get('tag')
+				submitted_loop.filename = filename # same filename of s3 bucket file
+				submitted_loop.save()
+
+
+			return redirect('/upload')
+
+		else:
+			return "uhoh there was an error " + uploaded_file.filename
+
+
+
+	else:
+		# get existing images
+		songs = models.Song.objects.order_by('timestamp')
+		
+		# render the template
+		templateData = {
+			'songs' : songs,
+			'form' : song_upload_form
+		}
+
+		return render_template("upload.html", **templateData)
+
+
 @app.route("/loops/add", methods=["POST"])
 def newloop():
 
@@ -378,10 +450,14 @@ def about():
 
 	return render_template("about.html")
 
-@app.route('/delete/<songid>')
-def delete_image(songid):
+@app.route('/delete/<live>')
+def delete_image(live):
 	
-	song = models.Song.objects.get(id=songid)
+	
+	song = models.Song.objects(tag=live)
+	
+
+
 	if song:
 
 		# delete from s3
@@ -410,7 +486,7 @@ def delete_image(songid):
 def data_loops():
 
 	# query for the ideas - return oldest first, limit 10
-	loops = models.Song.objects().order_by('-timestamp')
+	loops = models.Song.objects().order_by('timestamp')
 
 	if loops:
 
